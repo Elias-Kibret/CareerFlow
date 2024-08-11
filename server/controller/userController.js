9e6d06ff4806eef2e79baaf154e8fbd84e4d4cef
Abel h <abelhailu320@gmail.com>
2024-08-11 14:46:55 -0500

    authenticate midleware added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -77,7 +77,6 @@ export const login = async (req, res) => {
     const token = user.createJWT();
     res.status(200).json({ user, token });
   } catch (error) {
-    res.status(500).json({message:"Error logging in" ,error})
+    res.status(500).json({ message: "Error logging in", error });
   }
 };
-
